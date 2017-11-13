d3df84ebe5c99ad807726adece39564299e698bb
Graham Haley <graham.andrew.haley@gmail.com>
2017-11-13 11:06:07 -0800

    removed lingering local reference to http-status-codes that should've been removed when the project started using an npm package to handle status codes.  also removed useless npm test script

--- generators/app/templates/src/controllers/_default.ts
+++ generators/app/templates/src/controllers/_default.ts
@@ -1,7 +1,7 @@
 import { Request, Response, Router } from 'express';
 import { check, validationResult, ValidationChain } from 'express-validator/check';
 import { matchedData } from 'express-validator/filter';
-import { HttpStatus } from '../config/http-status-codes';
+import * as httpStatus from 'http-status-codes';
 
 export const defaultRouter: Router = Router();
 
@@ -14,7 +14,7 @@ export const getDefault = async (request: Request, response: Response) => {
     const errors: string[] = [];
     const message: {} = 'howdy!';
 
-    response.status(HttpStatus.Ok).send({ message, errors });
+    response.status(httpStatus.OK).send({ message, errors });
 };
 
 defaultRouter.get('/', getDefaultValidations, getDefault);
